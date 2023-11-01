0baf665bdbaa9add5f24f137b344f46b4e75d42f
Artem199627 <artemishko@gmail.com>
2023-11-01 19:40:21 +0100

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.body;
+ let timerId;
+
+btnStart.addEventListener("click", renderBackgroundColor);
+btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    btnStart.toggleAttribute("disabled");
+});
+
+function renderBackgroundColor()  {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStart.toggleAttribute("disabled");
+}
+
+ function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
